336819ce63032ffa89af97cd0894320e9d6c65ee
Uchiha <55316568+UchihaTHT@users.noreply.github.com>
2019-10-12 16:40:57 +0300

    Add files via upload

---
+++ Classödev.py
@@ -0,0 +1,33 @@
+class Exam(object):
+    def __init__(self,true,false):
+    	self.score = []
+		for sayi in range(sayi):
+		self.score.append(sayi)
+    def score(self):
+        puanlar = 100/(true + false)*true
+        self.score.append(puanlar)
+
+
+class Öğrenci(object):
+    def __init__(self, ogrenci, exam):
+        self.ogrenci = ogrenci
+        self.exam = exam
+
+    def ogrenci(self):
+		return self.ogrenci
+
+	def avg_score(self):
+    	return sum(self.score) / len(self.score)
+   
+class Sinif(Ogrenci):
+    def __init__(self,sinif,ort):
+        self.sinif = sinif
+        self.ort = ortalama
+    
+    def class_avg(self):
+    	return sum(self.score) / len(self.score)
+
+
+
+
+
